4123c574e6f5e656566f0be9f19568ed06fe23dd
Myrilandel <darkminie@Hotmail.fr>
2017-02-27 09:31:34 +0100

    A vérifier + déplacer dans Factory !

--- ContainerCollectable.cs
+++ ContainerCollectable.cs
@@ -1,17 +1,23 @@
-[SerializedField]
-GameObject depletedGameObject;
-[SerializedField]
-Text textField;
-private void respawnDepleted() {
-	Instantiate(depletedGameObject, this.gameobject.transform, this.gameobject.rotation);
-	Destroy(this.gameobject);
-}
+using UnityEngine;
 
-public void CollectObjectFromContainer() {
-	Debug.Log("Items Collected");
-	respawnDepleted();
-}
+public class ContainerCollectable : ItemsPickable {
+	/** Un container collectable est un prefab qui contient des ItemsPickable. 
+	 *  La différence avec un simple itemCollectable est que son prefab peut être remplacé par un autre momentanément(i.e. arbre à fruits -> arbre sans fruits -> respawn arbre a fruit),
+	 *  ou simplement rester en l'état (i.e. un coffre plein devient un coffre vide)
+	 *  Cette classe est une factory et doit être étendue et fonctionne (généralement) avec un ContainerDepleted.
+	**/
+	
+	// TODO VERIFIER LE SCOPE DES METHODES ET VARIABLES DANS ELEMENTS FILS //
+	[SerializeField]
+	private GameObject depletedGameObject;
 
-public Text getTextToDisplay() {
-	return textField;
+	private void respawnDepleted() {
+		Instantiate(depletedGameObject, this.gameobject.transform, this.gameobject.rotation);
+		Destroy(this.gameobject);
+	}
+
+	public void CollectObjectFromContainer() {
+		//TODO : Methode d'ajout des items collectés dans l'inventaire.
+		respawnDepleted();
+	}
 }
--- ContainerDepleted.cs
+++ ContainerDepleted.cs
@@ -1,20 +1,27 @@
-[SerializedField]
-GameObject collectacleGameObject;
+using UnityEngine;
 
-private float respawnTimeInSec = 180.0f;
+public class ContainerDepleted : MonoBehavior {
+/** ContainerDepleted fonctionne en coordination avec un ContainerCollectable. Il représente le containerCollectable dépourvu de ses ItemsPickable.
+ *  Comme le container Collectable, cette classe est une Factory qui doit être étendue. Le respawnTimeInSec sera modifié dans les gameObjects fils.
+**/
 
-public virtual void SetRespawnTime(float respawnTimeInSec) {
-	this.respawnTimeInSec = respawnTimeInSec;
-}
+// TODO VERIFIER LE SCOPE DES METHODES ET VARIABLES DANS ELEMENTS FILS //
+	[SerializeField]
+	private GameObject collectacleGameObject;
 
-private void Update() {
-	respawnTimeInSec -= 1.0 * Time.deltaTime();
-	
-	if(respawnTimeInSec <= 0)
-		respawnCollectable();
-}
+	[SerializeField]
+	private float respawnTimeInSec;
 
-private void respawnCollectable() {
-	Instantiate(collectacleGameObject, this.gameobject.transform, this.gameobject.rotation);
-	Destroy(this.gameobject);
+
+	private void Update() {
+		respawnTimeInSec -= 1.0 * Time.deltaTime();
+		
+		if(respawnTimeInSec <= 0)
+			respawnCollectable();
+	}
+
+	private void respawnCollectable() {
+		Instantiate(collectacleGameObject, this.gameobject.transform, this.gameobject.rotation);
+		Destroy(this.gameobject);
+	}
 }
